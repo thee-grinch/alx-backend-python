776b303a411235f128dfa7547df8ef3ca7a88cb3
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-04-30 23:32:02 +0300

    all tasks

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,9 +2,9 @@
 """Type-annotated function element_length
 that takes a list lst as argument and returns the
 length of the list as a tuple."""
-from typing import List, Tuple
+import typing
 
 
-def element_length(lst: List[str]) -> List[Tuple[int, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """Return a tuple of the length of a list and a list of the lengths of each element."""
     return (len(lst), [len(i) for i in lst])
