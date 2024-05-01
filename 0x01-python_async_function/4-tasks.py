61f443fb856c1231f1fd3792fa6a87ad97b36eb0
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-05-01 23:17:59 +0300

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """task waut random"""
+from typing import List
 
 
 task_wait_random = __import__('3-tasks').task_wait_random
-async def task_wait_n(n: int, max_delay: int) -> float:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """takes in 2 int arguments (n and max_delay) and returns a list of delays"""
     import asyncio
     tasks = [task_wait_random(max_delay) for _ in range(n)]
