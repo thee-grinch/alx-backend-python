995153cff6fb3ba76c88d417bd23eaffb5049ee8
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-05-01 23:27:54 +0300

    complete tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """a function that tasks a random number and waits for i"""
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """takes in 2 int arguments (n and max_delay)
     and returns a list of delays"""
     import asyncio
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """calculate the runtime of wait_n(n, max_delay)"""
 import time
+import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
     """measures the runtime of wait_n"""
     start = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end = time.time()
-    return end - start
+    return (end - start) / n
